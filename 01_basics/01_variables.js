96a92125db21f82d52094aa187d4572213e4d451
kaustubhsawant76 <145922083+kaustubhsawant76@users.noreply.github.com>
2023-12-26 13:59:59 +0000

    variables  learned

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId=14453 //cannot be redeclared and also cannot be changed in middle
+let accountEmail="kaustubhsawant76@gmail.com" //cannot be redeclared but can be changed in middle
+var accountPassword="123456" // can be redeclared and also can be changed in middle that is why not used nowardays
+accountCity="MUMBAI"
+
+accountEmail="vrushabh@gmail.com"
+accountPassword="189"
+accountCity="banglore"
+let acoountState; //if value is not given the output is undefined
+console.table({accountId,accountEmail,accountPassword,accountCity,acoountState}) //to display data in table form use console.table
